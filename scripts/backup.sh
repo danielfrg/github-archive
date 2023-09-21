2338e616e15fac3328a395ba60072b61e83470dc
Daniel Rodriguez <daniel@danielfrg.com>
2023-09-21 17:06:33 -0500

    Split by source and archived

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,24 +7,34 @@ TAR_REPOS=${TAR_REPOS:-1}
 ROOT_DIR=${ROOT_DIR:-"data"}
 TARGET_DIR=${TARGET_DIR:-""}
 
-# Internal
-file="repos.txt"
+# ---
+file_sources="sources.txt"
+file_archived="archived.txt"
+
+declare -A kinds
+kinds=( ["source"]="sources.txt" ["archive"]="archived.txt")
 
 # Get list of repos
-gh repo list --limit $LIMIT --json name $OWNER | jq -r '.[]."name"' > $file
+gh repo list --limit $LIMIT --json name $OWNER --archived | jq -r '.[]."name"' > $file_sources
+gh repo list --limit $LIMIT --json name $OWNER --no-archived | jq -r '.[]."name"' > $file_archived
 
-# Clone and Tar
-mkdir -p $ROOT_DIR/$TARGET_DIR/$OWNER
-while read repo; do
-    echo "Cloning $repo to $ROOT_DIR/$TARGET_DIR/$OWNER/$repo"
-    rm -rf $ROOT_DIR/$TARGET_DIR/$OWNER/$repo
-    gh repo clone $OWNER/$repo $ROOT_DIR/$TARGET_DIR/$OWNER/$repo -- --quiet
+rm -rf $ROOT_DIR/$TARGET_DIR
 
-    if [ $TAR_REPOS -eq 1 ]; then
-        echo "Taring $repo"
-        cd $ROOT_DIR/$TARGET_DIR/$OWNER
-        tar -czf $repo.tar.gz $repo
-        rm -rf $repo
-        cd -
-    fi
-done < "$file"
+for kind in "${!kinds[@]}"; do
+    # Clone and tar files
+    mkdir -p $ROOT_DIR/$TARGET_DIR/$OWNER/$kind
+    while read repo; do
+        target_dir=$ROOT_DIR/$TARGET_DIR/$OWNER/$kind/$repo
+        echo "Cloning $repo to $target_dir"
+        rm -rf $target_dir
+        gh repo clone $OWNER/$repo $target_dir -- --quiet
+
+        if [ $TAR_REPOS -eq 1 ]; then
+            echo "Taring $repo"
+            cd $ROOT_DIR/$TARGET_DIR/$OWNER/$kind
+            tar -czf $repo.tar.gz $repo
+            rm -rf $repo
+            cd -
+        fi
+    done < "${kinds[$kind]}"
+done
